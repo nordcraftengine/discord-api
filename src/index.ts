afc034a1916c2632b24531ecc5278baaa6d2f3ff
Marija Gerasimovska <marija@toddle.dev>
2024-10-24 13:35:37 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,18 @@
  * configurable interval:
  * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
  *
- * - Run `npm run dev` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { getNewData } from './discord-api';
-import { saveData } from './updateData';
+import { getNewData } from './discord-api'
+import { saveData } from '../supabase/updateData'
 
 export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
 	async scheduled(event, env, ctx): Promise<void> {
-		const { newTopics, newMessages, newUsers } = await getNewData(env);
+		const { newTopics, newMessages, newUsers } = await getNewData(env)
 
-		const t = await saveData(newTopics, newMessages, newUsers);
+		const t = await saveData(newTopics, newMessages, newUsers)
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
