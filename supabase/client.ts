5db443e15279fa2b70cc77ecb4455216726384e9
Marija Gerasimovska <marija@toddle.dev>
2024-10-25 13:03:41 +0200

    clean console

--- supabase/client.ts
+++ supabase/client.ts
@@ -2,6 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from './types'
 
 export const getSupabaseClient = (env: Env) => {
-	console.log(env)
 	return createClient<Database>(env.SUPABASE_API_URL, env.SUPABASE_ANON_KEY)
 }
