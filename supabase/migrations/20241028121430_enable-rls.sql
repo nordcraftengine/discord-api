5c43340ce9e256215f7403e465465f75b24c90e1
Marija Gerasimovska <marija@toddle.dev>
2024-10-28 14:15:17 +0100

    Add read policy for all tables

--- supabase/migrations/20241028121430_enable-rls.sql
+++ supabase/migrations/20241028121430_enable-rls.sql
@@ -9,3 +9,28 @@ ALTER TABLE mentions ENABLE ROW LEVEL SECURITY;
 ALTER TABLE reactions ENABLE ROW LEVEL SECURITY;
 
 ALTER TABLE attachments ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Users are visible to everyone." ON users
+    FOR SELECT TO anon
+        USING (TRUE);
+
+CREATE POLICY "Topics are visible to everyone." ON topics
+    FOR SELECT TO anon
+        USING (TRUE);
+
+CREATE POLICY "Messages are visible to everyone." ON messages
+    FOR SELECT TO anon
+        USING (TRUE);
+
+CREATE POLICY "Mentions are visible to everyone." ON mentions
+    FOR SELECT TO anon
+        USING (TRUE);
+
+CREATE POLICY "Reactions are visible to everyone." ON reactions
+    FOR SELECT TO anon
+        USING (TRUE);
+
+CREATE POLICY "Attachments are visible to everyone." ON attachments
+    FOR SELECT TO anon
+        USING (TRUE);
+
