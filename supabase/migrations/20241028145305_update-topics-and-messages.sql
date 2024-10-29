150621dc1dee5bf5e323eebdda562f8c0b9546c3
Marija Gerasimovska <marija@toddle.dev>
2024-10-29 09:55:25 +0100

    populate topic_message_id

--- supabase/migrations/20241028145305_update-topics-and-messages.sql
+++ supabase/migrations/20241028145305_update-topics-and-messages.sql
@@ -1,6 +1,14 @@
 ALTER TABLE topics
-    ADD COLUMN topic_message_id text NOT NULL;
+    ADD COLUMN topic_message_id text;
 
 ALTER TABLE messages
     ADD CONSTRAINT "message_reference_message_fkey" FOREIGN KEY (message_reference) REFERENCES messages(id) ON DELETE SET NULL;
+
+UPDATE
+    topics
+SET
+    topic_message_id = id;
+
+ALTER TABLE topics
+    ALTER COLUMN topic_message_id SET NOT NULL;
 
