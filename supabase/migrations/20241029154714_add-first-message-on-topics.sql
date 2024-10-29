569de77ba41580d7619f6180996ef792ac3747b7
Marija Gerasimovska <marija@toddle.dev>
2024-10-29 16:51:14 +0100

    Add a new migration and delete the failing one

--- supabase/migrations/20241029154714_add-first-message-on-topics.sql
+++ supabase/migrations/20241029154714_add-first-message-on-topics.sql
@@ -1,11 +1,6 @@
 ALTER TABLE topics
-    ADD COLUMN topic_message_id text REFERENCES messages(id) ON DELETE SET NULL;
+    ADD COLUMN first_message text;
 
 ALTER TABLE messages
     ADD CONSTRAINT "message_reference_message_fkey" FOREIGN KEY (message_reference) REFERENCES messages(id) ON DELETE SET NULL;
-
-UPDATE
-    topics
-SET
-    topic_message_id = id;
 
