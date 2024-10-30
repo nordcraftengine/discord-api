b37f0b2efa2ee1f401c90cd67748af40d5425c5e
Marija Gerasimovska <marija@toddle.dev>
2024-10-30 12:46:51 +0100

    Add an index

--- supabase/migrations/20241030103924_add-first_message_id-on-topics.sql
+++ supabase/migrations/20241030103924_add-first_message_id-on-topics.sql
@@ -1,6 +1,8 @@
 ALTER TABLE topics
     ADD COLUMN first_message_id text REFERENCES messages(id) ON DELETE SET NULL;
 
+CREATE INDEX topics_first_message_id ON topics(first_message_id);
+
 UPDATE
     topics
 SET
