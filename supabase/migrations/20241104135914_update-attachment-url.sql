fac8430df88e0454a91b134536d0b7c5e3aa35c1
Marija Gerasimovska <marija@toddle.dev>
2024-11-04 15:07:19 +0100

    formatter

--- supabase/migrations/20241104135914_update-attachment-url.sql
+++ supabase/migrations/20241104135914_update-attachment-url.sql
@@ -1,9 +1,18 @@
-UPDATE attachments
-SET url = 
-    (SELECT
-        CASE 
-            WHEN strpos(a2.url, '?') > 0 THEN SUBSTRING(a2.url, strpos(a2.url, '/attachments'), strpos(a2.url, '?') - strpos(a2.url, '/attachments'))
-            ELSE a2.url 
-        END
-    FROM attachments a2
-    WHERE attachments.id = a2.id)
+UPDATE
+    attachments
+SET
+    url = (
+        SELECT
+            CASE
+                WHEN strpos(a2.url, '?') > 0 THEN SUBSTRING(
+                    a2.url,
+                    strpos(a2.url, '/attachments'),
+                    strpos(a2.url, '?') - strpos(a2.url, '/attachments')
+                )
+                ELSE a2.url
+            END
+        FROM
+            attachments a2
+        WHERE
+            attachments.id = a2.id
+    )
